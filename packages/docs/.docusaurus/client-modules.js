41751ff48ca26286757d4d746f0c21fca1ba31ec
BigStack Analytics <dev@bigstack-analytics.com>
2025-09-10 01:12:32 +0700

    chore(clean): untrack venv, __pycache__, *.pyc, docs cache

--- packages/docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/sv/project/aiser-world/node_modules/infima/dist/css/default/default.css"),
-  require("/home/sv/project/aiser-world/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/sv/project/aiser-world/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/sv/project/aiser-world/packages/docs/src/css/custom.css"),
-];
